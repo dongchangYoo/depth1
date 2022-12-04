b2bf1ce9c6a1ba2f72d7278759705975a9dfc9c5
dongchangYoo <dongchang.yoo@pi-lab.io>
2022-12-04 21:32:56 +0900

    up

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="depth1",
     version="v0.0.1",
     package=find_packages,
-    install_requires=[],
+    install_requires=["depth2"],
     dependency_links=["git+https//github.com/dongchangYoo/depth2.git"]
 )
