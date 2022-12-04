48c18bf55ded89ff3cc3113590f02717cdac359d
dongchangYoo <dongchang.yoo@pi-lab.io>
2022-12-04 22:54:53 +0900

    complete

---
+++ main.py
@@ -0,0 +1,5 @@
+from depth1.func import hello_depth1
+
+
+if __name__ == "__main__":
+    hello_depth1()
--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name="depth1",
     version="v0.0.1",
     package=find_packages,
-    install_requires=[],
-    dependency_links=["git+https://github.com/dongchangYoo/depth2.git"]
+    install_requires=[
+        "depth2 @ git+https://github.com/dongchangYoo/depth2.git"
+    ],
+    dependency_links=[
+
+    ]
 )
